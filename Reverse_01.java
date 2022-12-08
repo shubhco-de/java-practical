c4dd710f2c2b32d28f7d6b385bb0d4adf8f7eff4
Shubh <100335234+shubhco-de@users.noreply.github.com>
2022-12-08 19:50:03 +0530

    1st practical

---
+++ Reverse_01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Reverse_01 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number you want to reverse : ");
+        int n = in.nextInt();
+        int reverse = 0;
+        int reminder ;
+        while (n!=0){
+            reminder =n%10;
+            reverse = reverse *10 + reminder;
+            n = n/10;
+        }
+        System.out.println("The reverse number is : "+reverse);
+    }
+}
